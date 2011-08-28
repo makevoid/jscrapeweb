58c9b39ce0285874737b56e870f2fcb17d0179bf
makevoid <makevoid@gmail.com>
2011-08-28 22:04:08 +0200

    updated gemfile, simplified the doc

--- public/js/main.js
+++ public/js/main.js
@@ -7,22 +7,21 @@ $(function(){
   // frame = jScrape.frame("#jsc_frame1")
   // frame.get(url)
   
-  
   // agent.get "http://www.youtube.com/watch?v=eXqPYte8tvc"
   //agent.get "http://mkvd.net"
 
 
 
 
-  $("#jsc_test1").click(function() {
-    var jscrape = new $.jScrape()
-    var url = "http://jquery.com"
-    jscrape.get(url, function(page){
-      var html = $(page.html).find("#jq-intro").html()
-      $("#jsc_frame1").html(html)
-    })    
-  })
-    
+  // $("#jsc_test1").click(function() {
+  //   var jscrape = new $.jScrape()
+  //   var url = "http://jquery.com"
+  //   jscrape.get(url, function(page){
+  //     var html = $(page.html).find("#jq-intro").html()
+  //     $("#jsc_frame1").html(html)
+  //   })    
+  // })
+  //   
 
   $("#demo2 form").submit(function() {
     var jscrape = new $.jScrape()
